07bcbd30b01262fda273bcd1ac5119f1982a61c0
Maciej Skrzypek <maciek.skrzypek [at] gmail.com>
2024-02-19 22:04:37 +0100

    main runs funtional tests

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
 mod cpu6502;
 use cpu6502::{Memory,CPU6502};
+use std::time::{Instant};
 
 fn main() {
 
         let mem = Memory::from_file("./tests/6502_functional_test.bin").unwrap();
         let mut cpu = CPU6502::new(mem);
         cpu.reset_at(0x0400);
-        cpu.enable_trace(128);
 
         let mut cnt = 0;
+        let now = Instant::now();
 
         loop{
+            cnt += 1;
             match cpu.run_single() {
+
                 Ok(_) => {},
                 Err(e) => {
+                    if e.pc == 0x3469{
+                        println!("Run {} instructions", cnt); //This test program loops here on success
+println!("
+                        ; S U C C E S S ************************************************
+                        ; -------------
+                                success         ;if you get here everything went well
+3469 : 4c6934          >        jmp *           ;test passed, no errors");
+                        break;
+                    }
                     cpu.show_cpu_debug();
                     println!("Error: {}", e);
                     break;
                 }
             };
-            cnt += 1;
         }
-        println!("Run {} instructions", cnt);
-        cpu.show_cpu_debug();
+        let elapsed_time = now.elapsed();
+        println!("Run {} instructions instructions per sec {}", cnt, cnt * 1000 / elapsed_time.as_millis());
 }
